8b5eaac4dd736d8559868170ea16946458f2e479
Kirill <kerlykiv@gmail.com>
2023-04-03 22:01:27 +0700

    +forms+index

--- djangosite/blog/forms.py
+++ djangosite/blog/forms.py
@@ -1,15 +1,27 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from  .models import *
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    slug = forms.SlugField(max_length=255, label='URL', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10, 'class': 'form-control'}),
-                              label='Статья')
-    is_published = forms.BooleanField(label='Публикация', required=False, initial=True,
-                                      widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(),
-                                 label='Категории',
-                                 empty_label='Категория не выбрана',
-                                 widget=forms.Select(attrs={'class': 'form-control'}))
+class AddPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = 'Категория не выбрана'
+
+    class Meta:
+        model = Blog
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10, 'class': 'form-control'}),
+            'photo': forms.FileInput(attrs={'class': 'form-control'}),
+            'is_published': forms.CheckboxInput(attrs={'class': 'form-check'}),
+            'cat': forms.Select(attrs={'class': 'form-control'})
+        }
+
+        def clean_title(self):
+            title = self.cleaned_data['title']
+            if len(title) > 200:
+                raise ValidationError('Длина превышает 200 символов')
+            return title
--- djangosite/blog/views.py
+++ djangosite/blog/views.py
@@ -28,14 +28,11 @@ def about(request):
 
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             #print(form.cleaned_data)
-            try:
-                Blog.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка отправления поста')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
     return render(request, 'blog/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
